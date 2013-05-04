772369d5a3d29f48b8c18ab9e3a71509f687be6d
eight <lampz_08@yahoo.com>
2013-05-04 12:48:30 +0800

    edited bootstrap/css/home.php

--- home.php
+++ home.php
@@ -17,7 +17,7 @@
             <p>Please type your question</p>
             <p>here and click "Send".</p>
             <div class="you"></div>            
-            <textarea rows="2" cols="500" class="input-small autogrow"></textarea></br>
+            <textarea rows="2" cols="500" class="input-small autogrow"></textarea>
             <button class="btn btn-mini btn-inverse" id="send">Send</button>
             <button class="btn btn-mini" id="cancel">Cancel</button>
          </div>
@@ -34,41 +34,21 @@
                            <div class="menu">Promos</div>
                            <div class="menu">Contact Us</div>
                         </div>
+                      <p2>Summer Promo</p2>
+                      <div id = "img">
+                          <img src="images/promo.jpg" alt="promo" height="500" width="300">
+                          <img src="images/promo2.JPG" alt="promo1" height="500" width="300">
+                          <img src="images/promo1.jpg" alt="promo2" height="700" width="300">
+                      </div>
+                              <p1> Introducing </p1>
+                              <p1>The New Endorser of My|Phone: </p1>
+                             <p1> <b> <u> Daniel Padilla</u></b></p1>
 
 
-                      <div class="mainPic" id="slideshow">
-                           <img src="images/img1.png" class="active" />
-                           <img src="images/img2.png"  />
-                           <img src="images/img3.png"  />
-                           <img src="images/img4.png"  />
-                           <img src="images/img5.png"  />
-                           <img src="images/img6.png"  />
-                           <img src="images/img7.png"  />
-                           <img src="images/img8.png"  />
-                           <img src="images/img9.png"  />
-
-
-                        </div>
-
-                 
-                  <br>
-                     <div class="product-view">
-            
-                        <div class="advertise" id="latest product">
-                           <div class="title">Latest Product</div>
-                        </div>       
-                        <br>
-                        <div class="advertise" id="top product">
-                           <div class="title">Top Product</div>
-                        </div>    
-                        <br>
-                        <div class="advertise" id="recommended product">
-                           <div class="title">Recommended Product</div>
-                        </div>   
                      </div>  
                   </div>  
                </div>
-      </div>
+
       
       
    </body>
