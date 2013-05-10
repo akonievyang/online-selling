4163a09370937618c3db2302dabfd732c1b90ee2
arnel <bons_bmeg04@yahoo.com>
2013-05-10 15:32:52 +0800

    added laptop.php

---
+++ laptop.php
@@ -0,0 +1,17 @@
+<!doctype HTML>
+    <html>
+<head>
+    <body>
+    <style>
+#main{
+    background-size: 1000px;
+    background-image: ;
+}
+    </style>
+    <div id ="main">
+        <h2>Acer-Aspire-A5560-7414</h2>
+        <img src="laptop/Acer-Aspire-A5560-7414.png">
+    </div>
+    </body>
+</head>
+    </html>
