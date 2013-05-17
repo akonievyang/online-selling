e5ba2b2bf4bf8e0b64dadaddfcd89c81e0a9f805
dadzky33 <dadzky33@gmail.com>
2013-05-18 07:19:19 +0800

    edited nah

--- Settings.php
+++ Settings.php
@@ -81,10 +81,8 @@
                     <li><a href="#tabs-2"> Profile Settings </a></li>
                     <li><a href="#tabs-3"> Personal Settings </a></li>
                 </ul>
+                <!---------Account-------->
                 <div id="tabs-1">
-                        <button class="btn btn-primary" type="button" style="float: right;" >
-                             Save
-                         </button>
                          <tr> <h1> Account </h1> </tr>
 
                         <label>UserName</label>
@@ -92,17 +90,20 @@
 
                         </p> <strong> Password </strong></p>
 
-                        <label> Current password </label>
+                        <label>Current password</label>
                         <input type="password" id="pass">
 
                         <form class="form-inline">
 
                            <input type="password" class="input-large" placeholder="New password">
                            <input type="password" class="input-large" placeholder="Confirm new password">
+                            <button class="btn btn-primary" type="submit" style="float: right;" >
+                                Save
+                            </button>
                         </form>
 
                 </div>
-
+                <!---------Profile--------->
                 <div id="tabs-2">
                     <h2> Profile </h2>
 
@@ -110,19 +111,24 @@
 
                 </div>
                 <div id="tabs-3">
-
+                  <!------Personal---------->
                     <h2> Personal </h2>
 
-                    <label> Firstname </label>
-                    <input type="text" id="firstname"/>
-                    <label>Middlename</label>
-                    <input type="text" id="middlename"/>
-                    <label>Lastname</label>
-                    <input type="text" id="lastname"/>
-                    <label>Address</label>
-                    <input type="text" id="address"/>
-                    <label>Age</label>
-                    <input type="">
+                    <form class="form-inline">
+
+                        <input type="text" class="input-large" placeholder="firstname"/>
+                        <input type="text" class="input-large" placeholder="middlename"/>
+                    </form>
+
+                    <form class="navbar-form">
+                        <input type="text" class="input-large" placeholder="lastname"/>
+                        <input type="text" class="input-large" placeholder="address"/>
+                        <input type="text" class="input-large" placeholder="age"/>
+                        <input type="text" class="input-large" placeholder="gender">
+                            <button class="btn btn-primary" type="submit" style="float: right;" >
+                                Save
+                        </button>
+                   </form>
 
                 </div>
             </div>
